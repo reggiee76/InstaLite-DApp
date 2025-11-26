4b0dea8dd4f51c3f2f98ac631c5e96dd499ec112
Maggie Hernandez <100039095+maggiehz@users.noreply.github.com>
2025-11-25 16:49:46 -0800

    Updated Contract with Comments and logic fix
    Added comments and altered tip function to allow only Creator users to receive tips.
    (Business, Basic, and Creator users can send tips, only Creator users can receive tips.)

--- instalite.sol
+++ instalite.sol
@@ -9,7 +9,7 @@ contract InstaLite {
     enum RequestStatus { None, Open, Completed, Cancelled }
     uint256 public constant MONTHLY_SUBSCRIPTION_PRICE = 0.01 ether;
 
-    struct Profile {        // Profile structure
+    struct Profile {        // Profile Structure
         string username;
         UserRole role;
         bool exists;
@@ -34,7 +34,7 @@ contract InstaLite {
         string description;
     }
 
-    // Current State
+    // Current State of the InstaLite Application
     uint256 public nextPostID = 1;
     uint256 public nextRequestID = 1;
 
@@ -59,10 +59,15 @@ contract InstaLite {
     event RequestCancelled(uint256 indexed requestID);
     event RequestCompleted(uint256 indexed requestID);
 
-    // Functions
+
+    // FUNCTIONS
+
+    // Function to Create/Update a Profile (Assign a username and role to an address)
     function createProfile(string calldata username, UserRole role) external {
+        // Revert the call if the role entered is not a valid role.
         require(role != UserRole.None, "Invalid role");
 
+        // Update the user's username, role, and assert that the account now exists.
         profiles[msg.sender] = Profile({
             username: username,
             role: role,
@@ -72,15 +77,24 @@ contract InstaLite {
         emit ProfileCreated(msg.sender, username, role);
     }
 
+    // Function to Create a Post ()
     function createPost(string calldata mediaURI, string calldata caption, bool isPremium) external returns (uint256) {
+        // Revert the call if profile does not exist.
         require(profiles[msg.sender].exists, "Profile required.");
+
+        // Revert if the user did not input the media URL.
         require(bytes(mediaURI).length > 0, "Media URI required.");
 
+        // Check if user is uploading premium content (subscriber-only content)
         if (isPremium) {
+            // Revert if the user does not have the Creator Role which is required for posting premium content.
             require(profiles[msg.sender].role == UserRole.Creator, "Only creators can post premium content.");
         }
 
+        // Update the global postID
         uint256 postID = nextPostID++;
+
+        // Update post parameters as given from user and initialize others
         posts[postID] = Post({
             postID: postID,
             author: msg.sender,
@@ -91,59 +105,96 @@ contract InstaLite {
             hidden: false
         });
 
+        // Update individual user posts by postID
         userPosts[msg.sender].push(postID);
 
         emit PostCreated(postID, msg.sender, mediaURI, caption, isPremium);
         return postID;
     }
 
+    // Function to Hide/Unhide a Post
     function hidePost(uint256 postID) external {
+        // Revert if the postID given does not exist.
         require(posts[postID].postID != 0, "Post does not exist.");
+
+        // Revert if the address trying to hide the post is not the post author.
         require(posts[postID].author == msg.sender, "Not post author.");
 
+        // Set post "hidden" attribute to the opposite boolean value. Hide (true) UnHide (false)
         posts[postID].hidden = !posts[postID].hidden;
 
         emit PostHidden(postID, msg.sender);
     }
 
+    // Function to Like a Post
     function likePost(uint256 postID) external {
+        // Revert if the postID given does not exist.
         require(posts[postID].postID != 0, "Post does not exist.");
+
+        // Revert if the post is hidden. Should not be able to like.
         require(!posts[postID].hidden, "Post is hidden.");
+
+        // Revert if the profile does not exist.
         require(profiles[msg.sender].exists, "Profile required.");
 
+        // Increase the Like Count for the given post ID
         likeCount[postID] += 1;
+
         emit PostLiked(postID, msg.sender);
     }
 
+    // Function to gather a list of Post ID's for a given user (address)
     function getUserPosts(address user) external view returns (uint256[] memory) {
         return userPosts[user];
     }
 
+    // Function to send a tip to a Creator User
     function tip(address to, uint256 postID) external payable {
+        // Revert if the sender user doesn't exist.
         require(profiles[msg.sender].exists, "Profile required.");
-        require(msg.value > 0, "No ETH sent.");
-        require(profiles[to].exists, "Recipient has no profile.");
-        require(to != address(0), "Invalid Recipient.");
 
+        // Revert if the ETH sent is not greater than 0.
+        require(msg.value > 0, "No ETH sent.");
+
+        // Revert if the recipient user does not exist.
+        require(profiles[to].exists, "Recipient has no profile.");
+
+        // Revert if the recipient address does not exist. (Probably not needed.)
+        // require(to != address(0), "Invalid Recipient.");
+
+        // Revert if the recipient profile role is not a Creator.
+        require(profiles[to].role == UserRole.Creator, "Only creator users can be tipped.")
+
+        // Send tip to recipient and check for success using bool var "sent".
         (bool sent, ) = payable(to).call{value: msg.value}("");
         require(sent, "Transfer failed.");
 
         emit Tipped(msg.sender, to, msg.value, postID);
     }
 
+    // Function to subscribe to a Creator User
     function subscribe(address creator) external payable {
+        // Revert if the sender user profile does not exist.
         require(profiles[msg.sender].exists, "Profile required.");
-        require(profiles[creator].exists, "Creator has no profile.");
-        require(profiles[creator].role == UserRole.Creator, "Target is not a creator.");
 
+        // Revert if the creator user profile does not exist.
+        require(profiles[creator].exists, "Creator has no profile.");
+
+        // Revert if the recipient of the subscription is not a Creator User.
+        require(profiles[creator].role == UserRole.Creator, "Only creator users can be subscribed to.");
+
+        // Revert if the user is not sending the correct ETH ammount for a subscription purchase.
         require(msg.value == MONTHLY_SUBSCRIPTION_PRICE, "Wrong ETH amount.");
 
+        // Get the subscription expiration date currently on user account.
         uint256 currentExpireDate = subExpiry[creator][msg.sender];
 
+        // Update the subscription expiration date to reflect the current date.
         if (currentExpireDate < block.timestamp) {
             currentExpireDate = block.timestamp;
         }
 
+        // Calculate and update the new Sub Expiration date.
         uint256 newExpiry = currentExpireDate + 30 days;
         subExpiry[creator][msg.sender] = newExpiry;
 
@@ -153,19 +204,32 @@ contract InstaLite {
         emit Subscribed(msg.sender, creator, newExpiry);
     }
 
+    // Function to check if a user has an active subscription to a creator.
     function hasActiveSubscription(address creator, address subscriber) external view returns (bool) {
         return subExpiry[creator][subscriber] >= block.timestamp;
     }
 
+    // Function for Business Users to create a request to a Creator User for paid (advertised) content.
     function createRequest(address creator, string calldata description) external payable {
+        // Revert if the address sender does not have a profile.
         require(profiles[msg.sender].exists, "Profile required.");
+
+        // Revert if the sender does not have a Business Profile.
         require(profiles[msg.sender].role == UserRole.Business, "Only business profiles can make this request.");
 
+        // Revert if the ETH payment is not greater than 0.
         require(msg.value > 0, "No ETH sent");
-        require(profiles[creator].exists, "Creator has no profile.");
+
+        // Revert if the recipient address does not have a profile.
+        require(profiles[creator].exists, "Recipient has no profile.");
+
+        // Revert if the recipient is not a Creator Profile.
         require(profiles[creator].role == UserRole.Creator, "Target not a creator.");
 
+        // Update global requestID.
         uint256 requestID = nextRequestID++;
+
+        // Create the new request with information provided by the Business and initialize status variables.
         requests[requestID] = Request({
             requestID: requestID,
             business: msg.sender,
@@ -178,17 +242,28 @@ contract InstaLite {
         emit RequestCreated(requestID, msg.sender, creator, msg.value, description);
     }
 
+    // Function for marking a request as Completed. Creator gets paid and Business sends payment.
     function completeRequest(uint256 requestID) external {
+        // Store the requestID information
         Request storage r = requests[requestID];
+
+        // Revert if the requestID does not exist.
         require(r.requestID != 0, "Request does not exist");
+
+        // Revert if the request staus is still open.
         require(r.status == RequestStatus.Open, "Not open");
+
+        // Revert if the sender is not the user creator completing the request.
         require(msg.sender == r.creator, "Not your request");
 
+        // Set the request status as completed.
         r.status = RequestStatus.Completed;
 
+        // Save the payment amount and reset the value.
         uint256 amount = r.amount;
         r.amount = 0;
 
+        // Send funds to recipient and check for success using bool var "sent".
         (bool sent, ) = payable(r.creator).call{value: amount}("");
         require(sent, "Payout failed");
 
